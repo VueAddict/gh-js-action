697b16574606a27616df40c7d84236ba6c119b2d
Lockdown <75474296+VueAddict@users.noreply.github.com>
2025-08-07 20:36:58 +0500

    feat: create js action

---
+++ index.js
@@ -0,0 +1,22 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+    try {
+           // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const num1 = core.getInput('number1');
+    const num2 = core.getInput('number1');
+
+    const result = Number(num1) + Number(num2)
+
+   core.setOutput('result', result)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+
+}
+
+run();
